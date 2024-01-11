38f7d4cabba6b37ce7d3757f5266674a8213a027
tiaz0128 <tiaz0128.dev@gmail.com>
2024-01-11 21:58:04 +0900

    fix

--- src/study/to_be/week_2/solution_3.py
+++ src/study/to_be/week_2/solution_3.py
@@ -1,30 +1,26 @@
-def solution(grid):
-    # 행 row
-    n = len(grid)
+def solution(grid: list[list[int]]) -> list[list[int]]:
+    n, m = len(grid), len(grid[0])
 
-    # 열 col
-    m = len(grid[0])
+    def is_valid_cell(y, x):
+        return 0 <= y < n and 0 <= x < m
+
+    def update_adjacent_cells(i, j):
+        range_xy = [-1, 0, 1]
+        for y in range_xy:
+            for x in range_xy:
+                if not (x == 0 and y == 0):
+                    target_y, target_x = i + y, j + x
+                    if (
+                        is_valid_cell(target_y, target_x)
+                        and grid[target_y][target_x] != -1
+                    ):
+                        grid[target_y][target_x] += 1
 
     for i in range(n):
         for j in range(m):
             cell = grid[i][j]
-            print(i, j, cell)
 
             if cell == -1:
-                wave_plus_one(grid, n, m, i, j)
+                update_adjacent_cells(i, j)
 
     return grid
-
-
-def wave_plus_one(grid, n, m, i, j):
-    # 범위
-    range_xy = [-1, 0, 1]
-
-    for y in range_xy:
-        for x in range_xy:
-            if not (x == 0 and y == 0):
-                target_y = i + y
-                target_x = j + x
-                if target_y >= 0 and target_y < n and target_x >= 0 and target_x < m:
-                    if grid[target_y][target_x] != -1:
-                        grid[target_y][target_x] += 1
