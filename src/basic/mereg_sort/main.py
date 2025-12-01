39a873bb6f40d726071e9fcea7215b61bf72ff8a
tiaz0128 <tiaz0128.dev@gmail.com>
2025-12-01 13:22:02 +0900

    merge sort

---
+++ src/basic/mereg_sort/main.py
@@ -0,0 +1,36 @@
+def merge(left, right):
+    merged = []
+    i, j = 0, 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+
+    return merged
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = arr[:mid]
+    right = arr[mid:]
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge(left, right)
+
+
+data = [12, 11, 13, 5, 6, 7]
+sorted_data = merge_sort(data)
+
+print(sorted_data)
